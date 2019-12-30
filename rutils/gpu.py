1876c915fde3961f0d3e29502366e577ea5a9205
Rui Liu <ruixliu@umich.edu>
2019-12-30 17:09:23 -0500

    changed to remove the tmp file for get_free_gpu

--- rutils/gpu.py
+++ rutils/gpu.py
@@ -11,7 +11,11 @@ def get_free_gpu():
     Returns:
     - idx: an integer scalar which is the index of the returned GPU 
     """
-    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp_gpu')
+    tmp_file = 'tmp_gpu'
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >' + tmp_file)
     memory_available = [int(x.split()[2]) for x in open('tmp_gpu', 'r').readlines()]
+    os.system('rm '+tmp_file)
+    if len(memory_available) == 0:
+      return None # no gpu available
     idx =  np.argmax(memory_available)
     return idx
