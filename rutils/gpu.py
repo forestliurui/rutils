cd387ab36224eda634e035e041411c44aba35b5d
Rui Liu <ruixliu@umich.edu>
2019-12-30 23:38:13 -0500

    changed to choose gpu without process running first

--- rutils/gpu.py
+++ rutils/gpu.py
@@ -13,9 +13,18 @@ def get_free_gpu():
     """
     tmp_file = 'tmp_gpu'
     os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >' + tmp_file)
-    memory_available = [int(x.split()[2]) for x in open('tmp_gpu', 'r').readlines()]
+    memory_available = [int(x.split()[2]) for x in open(tmp_file, 'r').readlines()]
     os.system('rm '+tmp_file)
+
+    os.system('nvidia-smi -q|grep Processes>'+tmp_file)
+    proc_available = [len(x.split())>=3 for x in open(tmp_file, 'r').readlines()]
+    os.system('rm '+tmp_file)
+
+
     if len(memory_available) == 0:
       return None # no gpu available
-    idx =  np.argmax(memory_available)
+
+    stat_list = [(proc_available[idx], memory_available[idx], idx) for idx in range(len(proc_available))]
+    #idx =  np.argmax(memory_available)
+    idx = sorted(stat_list, reverse=True)[0][2]
     return int(idx)
