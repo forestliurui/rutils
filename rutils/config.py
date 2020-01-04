246d3104d69ba8911846f8273d2d1435d5cc251d
Rui Liu <ruixliu@umich.edu>
2020-01-03 22:27:05 -0500

    changed to add indent to json dump

--- rutils/config.py
+++ rutils/config.py
@@ -1,7 +1,7 @@
 import json
 
 def dict_to_json_file(dict_obj, file_name):
-    json.dump(dict_obj, open(file_name, 'w'))
+    json.dump(dict_obj, open(file_name, 'w'), indent=2)
 
 def json_file_to_dict(file_name):
     dict_obj = json.load(open(file_name, 'r'))
