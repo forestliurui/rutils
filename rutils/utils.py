8216946d4dc72f64b4bc6d12b35316a3c4bd7f0e
Rui Liu <ruixliu@umich.edu>
2020-03-07 16:05:08 -0500

    added functions for dataset partition

--- rutils/utils.py
+++ rutils/utils.py
@@ -100,3 +100,57 @@ def smoothen(curve_list, window_size=5):
     return result_list
 
 
+class Partition(object):
+    """
+    One partition of the dataset with interface compatible with pytorch when in distributed setting
+    i.e., with support of __len__ and __getitem__ functions
+    """
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
+
+
+class DataPartitioner(object):
+    """
+    Used to partition a dataset into several nonoverlapping partitions.
+    This is normally used in distributed training when each machine wants to access a nonoverlapping partition of the entire dataset.
+
+    Example:
+    dataset = datasets.MNIST('./data', train=True, download=True,
+                             transform=transforms.Compose([
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.1307,), (0.3081,))
+                             ]))
+    size = dist.get_world_size()
+    bsz = 128 / float(size)
+    partition_sizes = [1.0 / size for _ in range(size)]
+    partition = DataPartitioner(dataset, partition_sizes)
+    partition = partition.use(dist.get_rank())
+    train_set = torch.utils.data.DataLoader(partition,
+                                         batch_size=bsz,
+                                         shuffle=True)
+    """
+    def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
+        self.data = data
+        self.partitions = []
+        rng = Random()
+        rng.seed(seed)
+        data_len = len(data)
+        indexes = [x for x in range(0, data_len)]
+        rng.shuffle(indexes)
+
+        for frac in sizes:
+            part_len = int(frac * data_len)
+            self.partitions.append(indexes[0:part_len])
+            indexes = indexes[part_len:]
+
+    def use(self, partition):
+        return Partition(self.data, self.partitions[partition])
+
