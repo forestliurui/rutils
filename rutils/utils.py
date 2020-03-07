95a81e0e885e9a7f2f99a810d2dfab08b949accb
Rui Liu <ruixliu@umich.edu>
2020-03-07 16:56:37 -0500

    fixed a bug

--- rutils/utils.py
+++ rutils/utils.py
@@ -140,11 +140,10 @@ class DataPartitioner(object):
     def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
         self.data = data
         self.partitions = []
-        rng = Random()
-        rng.seed(seed)
+        random.seed(seed)
         data_len = len(data)
         indexes = [x for x in range(0, data_len)]
-        rng.shuffle(indexes)
+        random.shuffle(indexes)
 
         for frac in sizes:
             part_len = int(frac * data_len)
